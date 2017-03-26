b47011d030f9706c9cf2071a59f732d3c180f26b
VitorSoaresSilva <vitorcabeca2005@hotmail.com>
2017-03-25 21:43:10 -0300

    Update index.js

--- System/js/index.js
+++ System/js/index.js
@@ -2,4 +2,32 @@
 $.get('base/D_LOTMAN.HTM', function(data) {
 	//Trabalhe com os dados aq, diretamente do arquivo base
      $('#conteudo').html(data);
+     window.setTimeout( function(){
+     	var array = [];
+		var table = $('table');
+		var chave = [];
+		 	table.find('tr').each(function(itr){
+		 		array = [];
+		 		//alert("teste"+itr);
+		 		//if(itr < 5){
+		 			$(this).find('th').each(function(){
+		 				chave.push($(this).text());
+		 			})
+
+					$(this).find('td').each(function(itd){
+						if(itd <= 24 || itd == 32){
+							array[itd] = $(this).text();
+						}else if(itd == 33){
+							console.log(array)
+							/**$.post("teste.php", {
+								"data": array
+							});/**.done(function(retorno){
+								//alert(retorno);
+							});**/
+						}
+				 	});
+				//}
+		 	});
+	 	
+	 },5000);
 });
