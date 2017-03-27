fd363eedc65f84bc897dbfaf6cf5bceee901d1a1
Guilherme <guilherme@getconsultoria.com.br>
2017-03-26 23:43:57 -0300

    Ajuste de json para retorno dos concursos

--- System/js/index.js
+++ System/js/index.js
@@ -1,39 +1,53 @@
 	$(document).ready(function () {
         var chavesBuscadas = ['Data Sorteio'];
         var array = [];
+        var aux = [];
         var table = $('table');
-        var chave = [];
+        chave = [];
         var concursos = 0;
 
+
         table.find('tr').each(function(itr){
-            array = [];
+
+                $(this).find('th').each(function(ith){
+                    chave[ith] = $(this).text();
+                });
+        });
+
+        table.find('tr').each(function(itr){
+            aux = [];
+            /** Só entra caso haja ganhado único com 20 números **/
             if($("tr:nth-child("+(itr+1)+") td:nth-child(24)").text() == 1){
             	++concursos;
                 $(this).find('td').each(function(itd){
                     if(itd <= 24 || itd == 32){
-                        array[itd] = $(this).text();
-                    }else if(itd == 33){
-                        console.log(array)
+                        pushToAry(chave[itd],$(this).text() )
+                    }
                         /**$.post("teste.php", {
 								"data": array
 							});/**.done(function(retorno){
 								//alert(retorno);
 							});**/
-                    }
                 });
-
+                array.push(aux);
             }
-            else{
-               // $("tr:nth-child("+itr+")").remove();
 
-            }
+            if(concursos == 10)
+                return false;
+
 
         });
-        console.log(concursos);
+
+        console.log(JSON.stringify(array))
 
 
 
-
+        /** Função para retorno de array de objetos **/
+        function pushToAry(name, val) {
+            var obj = {};
+            obj[name] = val;
+            aux.push(obj);
+        }
 
 
     });
